df607ca97e0a20cfc7b4dd02bcf2ad6a2f5e3bf8
Guilherme <guilherme.bort@outlook.com>
2019-08-15 12:16:53 -0300

    add Test

--- ManualisMissing/Assets/Source/TestScript.cs
+++ ManualisMissing/Assets/Source/TestScript.cs
@@ -1,5 +1,12 @@
 ﻿using UnityEngine;
 
-public class TestScript : MonoBehaviour {
-
+public class TestScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("funciona!");
+        }
+    }
 }
